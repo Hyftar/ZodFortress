53a0a0b3a172e6977d83c089b254ac5f5eea1c80
FFA702 <urator.ar@hotmail.com>
2015-08-30 01:09:20 -0400

    some shit

---
+++ ZodFortress/Engine/Units/UnitType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZodFortress.Engine.Units
+{
+    public enum UnitType
+    {
+        Rock,
+        Grass,
+        Water,
+        Lava,
+        Road,
+        WoodWall,
+        StoneWall,
+        Tree,
+        Floor
+    }
+}
