6493c63be3acd7e39f41a1ac354e97eac5d3d72a
Hyftar <simon_landry@ymail.com>
2015-09-09 17:40:03 -0400

    Standadize test class

--- ZodFortressUnitTest/MapGenerationTest.cs
+++ ZodFortressUnitTest/MapGenerationTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Drawing;
+using System.Diagnostics;
 using ZodFortress.Engine;
 using ZodFortress.Engine.Units;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,17 +11,29 @@ namespace ZodFortressUnitTest
     [TestClass]
     public class MapGenerationTest
     {
+        private Map map;
+        private MapGenerator mapGen;
+
+        [TestInitialize]
+        public void TestInitializer()
+        {
+            Trace.WriteLine("Creating default map...");
+            map = new Map(new Size(100, 100), 1, MapGenerator.Grass);
+
+            Trace.WriteLine("Generating terrain...");
+            mapGen = new MapGenerator(map);
+
+            Assert.IsInstanceOfType(map, typeof(Map), "Failed to initialize map.");
+            Assert.IsInstanceOfType(mapGen, typeof(MapGenerator), "Failed to initialize the map generator.");
+        }
         /// <summary>
         /// Asserts that the map is populated with board blocks by default
         /// </summary>
         [TestMethod]
         public void GenerateDefaultMap()
         {
-            System.Diagnostics.Trace.WriteLine("Creating default map...");
-            var map = new Map(new Size(100, 100), 1, MapGenerator.Grass);
-            System.Diagnostics.Trace.WriteLine("Generating terrain...");
-            var mapg = new MapGenerator(map);
-            mapg.Generate(map.Layers.First());
+
+            mapGen.Generate(map.Layers.First());
 
             int trees = 0;
             int grass = 0;
@@ -42,7 +55,7 @@ namespace ZodFortressUnitTest
                 }
             }
 
-            System.Diagnostics.Trace.WriteLine(string.Format("Generated {0} grass, {1} trees, {2} rocks. Total: {3}.", grass, trees, rocks, total));
+            Trace.WriteLine(string.Format("Generated {0} grass, {1} trees, {2} rocks. Total: {3}.", grass, trees, rocks, total));
         }
     }
 }
