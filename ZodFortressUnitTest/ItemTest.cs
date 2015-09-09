2e9025072d19607dbb735620fd1165e450c878d5
Hyftar <simon_landry@ymail.com>
2015-09-09 17:37:53 -0400

    Add test class, rename PlayerExperience.cs to PlayerExperienceTest.cs

---
+++ ZodFortressUnitTest/ItemTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Drawing;
+using System.Diagnostics;
+using ZodFortress;
+using ZodFortress.Engine;
+using ZodFortress.Engine.Items;
+using ZodFortress.Engine.Units;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZodFortressUnitTest
+{
+    [TestClass]
+    public class ItemTest
+    {
+        private Map map;
+        private Player player;
+        private Item sword;
+
+        [TestInitialize]
+        public void TestInitializer()
+        {
+            Trace.WriteLine("Creating default map...");
+            map = new Map(new Size(5, 5), 1, MapGenerator.Grass);
+
+            Trace.WriteLine("Creating default player...");
+            player = new Player(map.Layers.First(), new Point(5));
+
+            Trace.WriteLine("Creating default mob...");
+            sword = new Item("Test sword", 5, 10, 1.5, 1, EquipSlot.Attack);
+
+            Assert.IsInstanceOfType(map.GetType(), typeof(Map), "Failed to initialize map.");
+            Assert.IsInstanceOfType(player.GetType(), typeof(Player), "Failed to initialize player.");
+            Assert.IsInstanceOfType(sword.GetType(), typeof(Item), "Failed to initialize item.");
+        }
+
+        [TestMethod]
+        public void EquipItem()
+        {
+            Trace.WriteLine(string.Format("Adding {0} to inventory.", sword.Name));
+            player.Inventory.Add(sword);
+            Assert.IsTrue(player.Inventory.Any(), "Failed to add item to inventory.");
+
+            Trace.WriteLine(string.Format("Adding {0} to offensive slot.", sword.Name));
+            player.EquipItem(player.Inventory.First(), EquipSlot.Attack);
+            Assert.IsTrue(player.Inventory.Any(), "Failed to remove item from player's inventory.");
+            Assert.IsInstanceOfType(player.OffensiveSlot, typeof(Item), "Failed to add item to player's offensive slot.");
+        }
+    }
+}
---
+++ ZodFortressUnitTest/PlayerExperienceTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using ZodFortress;
+using ZodFortress.Engine;
+using ZodFortress.Engine.Units;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZodFortressUnitTest
+{
+    [TestClass]
+    public class PlayerExperienceTest
+    {
+        private Map map;
+        private Player player;
+        private Mob mob;
+
+        [TestInitialize]
+        public void TestInitializer()
+        {
+
+            Trace.WriteLine("Creating default map...");
+            map = new Map(new Size(100, 100), 1, MapGenerator.Grass);
+
+            Trace.WriteLine("Creating default player...");
+            player = new Player(map.Layers.First(), new Point(50, 50));
+
+            Trace.WriteLine("Creating default mob...");
+            mob = new Mob("TestMob", map.Layers.First(), new Point(1, 1), null, null, 2, 1, 0, 15, 'M', ConsoleColor.Black, MobType.Orc);
+
+            Assert.IsInstanceOfType(map, typeof(Map), "Failed to initialize the map.");
+            Assert.IsInstanceOfType(player, typeof(Player), "Failed to initiaze the player.");
+            Assert.IsInstanceOfType(mob, typeof(Mob), "Failed to initialize the mob.");
+        }
+
+        [TestMethod]
+        public void ExpToNextLevel()
+        {
+            Trace.WriteLine("Creating default map...");
+            Trace.WriteLine("Creating player...");
+            Trace.WriteLine("Giving 130 experience to the player...");
+            player.GiveExperience(130);
+            Trace.WriteLine(string.Format("The player is currently level {0}, has {1} exp, {2} HP, {3} attack, {4} defense.", 
+                player.Level, 
+                player.Experience, 
+                player.Health, 
+                player.AttackStat, 
+                player.DefenseStat));
+            Assert.AreEqual(6, player.Level);
+        }
+    }
+}
